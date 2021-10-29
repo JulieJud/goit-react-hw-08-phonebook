c00a48afb0045541cd3e101aa6334d83aae8998d
Julie Judachina <iuliia.saunova@email.cz>
2021-10-29 10:29:08 +0200

    index

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
+import { BrowserRouter } from 'react-router-dom';
 import App from './components/App/App';
-import './index.css';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../src/index.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
