6d9732e41b827365d0fea36a4543eea2f154835d
Julie Judachina <iuliia.saunova@email.cz>
2021-10-29 10:19:19 +0200

    reducers

--- src/redux/phonebook/phonebook-reducers.js
+++ src/redux/phonebook/phonebook-reducers.js
@@ -8,18 +8,11 @@ import {
 } from './phonebook-operations';
 
 const entities = createReducer([], {
-  [fetchContactsAction.fulfilled]: (_state, action) => action.payload,
-  [addContactAction.fulfilled]: (state, { payload }) => {
-    if (
-      state.find(
-        contact => contact.name.toLowerCase() === payload.name.toLowerCase(),
-      )
-    ) {
-      alert(`${payload.name} is already in contacts`);
-      return state;
-    }
-    return [payload, ...state];
-  },
+  [fetchContactsAction.fulfilled]: (_state, action) => action.payload.data,
+  [addContactAction.fulfilled]: (state, { payload }) => [
+    payload.data,
+    ...state,
+  ],
   [deleteContactAction.fulfilled]: (state, { payload }) =>
     state.filter(({ id }) => id !== payload),
 });
