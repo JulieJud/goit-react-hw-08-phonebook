732170c6719514d6f2e82e664a66886e686a7afd
Julie Judachina <iuliia.saunova@email.cz>
2021-10-29 10:41:20 +0200

    appBar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,71 @@
-import ContactList from '../contacts/ContactsList/ContactsList.jsx';
-import ContactForm from '../contacts/ContactsForm/ContactsForm.jsx';
-import Filter from '../Filter/Filter.jsx';
-import { Title } from './App.styled';
+import { useEffect, lazy, Suspense } from 'react';
+import { Switch } from 'react-router';
+import { useDispatch, useSelector } from 'react-redux';
+import Loader from 'react-loader-spinner';
+import PrivateRoute from '../PrivateRoute';
+import PublicRoute from '../PublicRouter';
+import AppBar from '../AppBar/AppBar';
+import { fetchCurrentUser } from '../../redux/auth/auth-operations';
+import authSelectors from '../../redux/auth/auth-selectors';
+import styles from '../AppBar/AppBar.module.css';
+
+const AsyncHomeView = lazy(() =>
+  import('../../views/HomeView' /* webpackChunkName: "home-page"*/),
+);
+const AsyncLoginView = lazy(() =>
+  import('../../views/LoginView' /* webpackChunkName: "login-page"*/),
+);
+const AsyncRegisterView = lazy(() =>
+  import('../../views/RegisterView' /* webpackChunkName: "register-page"*/),
+);
+const AsyncContactsView = lazy(() =>
+  import('../../views/ContactsView' /* webpackChunkName: "contacts-page"*/),
+);
 
 export default function App() {
-  return (
+  const dispatch = useDispatch();
+  const isFetching = useSelector(authSelectors.getIsFetching);
+
+  useEffect(() => {
+    dispatch(fetchCurrentUser());
+  }, [dispatch]);
+
+  return !isFetching ? (
     <div>
-      <Title>Phonebook</Title>
-      <ContactForm />
-      <Title>Contacts</Title>
-      <Filter />
-      <ContactList />
+      <AppBar />
+
+      <Suspense
+        fallback={
+          <Loader
+            type="ThreeDots"
+            color="#00BFFF"
+            height={80}
+            width={80}
+            timeout={3000}
+            display="flex"
+            justify-content="center"
+            className={styles.loader}
+          />
+        }
+      >
+        <Switch>
+          <PublicRoute exact path="/">
+            <AsyncHomeView />
+          </PublicRoute>
+
+          <PublicRoute exact path="/register" redirectTo="/contacts" restricted>
+            <AsyncRegisterView />
+          </PublicRoute>
+
+          <PublicRoute exact path="/login" redirectTo="/contacts" restricted>
+            <AsyncLoginView />
+          </PublicRoute>
+
+          <PrivateRoute path="/contacts" redirectTo="/login">
+            <AsyncContactsView />
+          </PrivateRoute>
+        </Switch>
+      </Suspense>
     </div>
-  );
+  ) : null;
 }
